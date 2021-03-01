5151604620b90e34e4848c466b4bf957dca5f322
Mark Miller <constenum@gmail.com>
2021-03-01 10:32:47 -0500

    added user login seeder data

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert(['first_name' => 'Kezia', 'last_name' => 'Adjebeng', 'student_number' => '2025368', 'email' => 'kadjebeng25@toledosua.org', 'password' => Hash::make('25368Day@'),]);
+        DB::table('users')->insert(['first_name' => 'Alexis', 'last_name' => 'Allegrini', 'student_number' => '2025300', 'email' => 'aallegrini25@toledosua.org', 'password' => Hash::make('25300Ran@'),]);
+        DB::table('users')->insert(['first_name' => 'Veronica', 'last_name' => 'Amaya', 'student_number' => '2025301', 'email' => 'vamaya25@toledosua.org', 'password' => Hash::make('25301Jar@'),]);
+        DB::table('users')->insert(['first_name' => 'Lilli', 'last_name' => 'Areddy', 'student_number' => '2025369', 'email' => 'lareddy25@toledosua.org', 'password' => Hash::make('25369One*'),]);
+        DB::table('users')->insert(['first_name' => 'Nevaeh', 'last_name' => 'Baker', 'student_number' => '2025302', 'email' => 'nbaker25@toledosua.org', 'password' => Hash::make('25302Ran&'),]);
+        DB::table('users')->insert(['first_name' => 'Elliette', 'last_name' => 'Bartlett', 'student_number' => '2025303', 'email' => 'ebartlett25@toledosua.org', 'password' => Hash::make('25303Dub%'),]);
+        DB::table('users')->insert(['first_name' => 'Kennedy', 'last_name' => 'Bethel', 'student_number' => '2025370', 'email' => 'kbethel25@toledosua.org', 'password' => Hash::make('25370Red$'),]);
+        DB::table('users')->insert(['first_name' => 'Maria', 'last_name' => 'Bonnell', 'student_number' => '2025304', 'email' => 'mbonnell25@toledosua.org', 'password' => Hash::make('25304Can*'),]);
+        DB::table('users')->insert(['first_name' => 'Catherine', 'last_name' => 'Brunsman', 'student_number' => '2025305', 'email' => 'cbrunsman25@toledosua.org', 'password' => Hash::make('25305Bam$'),]);
+        DB::table('users')->insert(['first_name' => 'Addison', 'last_name' => 'Brzuchalski', 'student_number' => '2025306', 'email' => 'abrzuchalski25@toledosua.org', 'password' => Hash::make('25306Sum&'),]);
+        DB::table('users')->insert(['first_name' => 'Emily', 'last_name' => 'Buerk', 'student_number' => '2025307', 'email' => 'ebuerk25@toledosua.org', 'password' => Hash::make('25307Fix&'),]);
+        DB::table('users')->insert(['first_name' => 'Mikel', 'last_name' => 'Byrd', 'student_number' => '2025308', 'email' => 'mbyrd25@toledosua.org', 'password' => Hash::make('25308Aba*'),]);
+        DB::table('users')->insert(['first_name' => 'Lydia', 'last_name' => 'Carroll', 'student_number' => '2025309', 'email' => 'lcarroll25@toledosua.org', 'password' => Hash::make('25309Tap$'),]);
+        DB::table('users')->insert(['first_name' => 'Carly', 'last_name' => 'Carter', 'student_number' => '2025310', 'email' => 'ccarter25@toledosua.org', 'password' => Hash::make('25310Sat*'),]);
+        DB::table('users')->insert(['first_name' => 'Olivia', 'last_name' => 'Colella', 'student_number' => '2025311', 'email' => 'ocolella25@toledosua.org', 'password' => Hash::make('25311Wet#'),]);
+        DB::table('users')->insert(['first_name' => 'Tessa', 'last_name' => 'Coutcher', 'student_number' => '2025312', 'email' => 'tcoutcher25@toledosua.org', 'password' => Hash::make('25312Pat&'),]);
+        DB::table('users')->insert(['first_name' => 'Ella', 'last_name' => 'Deaton', 'student_number' => '2025371', 'email' => 'edeaton25@toledosua.org', 'password' => Hash::make('25371Bog%'),]);
+        DB::table('users')->insert(['first_name' => 'Sloan', 'last_name' => 'DeWood', 'student_number' => '2025313', 'email' => 'sdewood25@toledosua.org', 'password' => Hash::make('25313Dad@'),]);
+        DB::table('users')->insert(['first_name' => 'Aubrey', 'last_name' => 'Diller', 'student_number' => '2025314', 'email' => 'adiller25@toledosua.org', 'password' => Hash::make('25314Elm@'),]);
+        DB::table('users')->insert(['first_name' => 'Gracie', 'last_name' => 'Espino', 'student_number' => '2025315', 'email' => 'gespino25@toledosua.org', 'password' => Hash::make('25315Rye%'),]);
+        DB::table('users')->insert(['first_name' => 'Avery', 'last_name' => 'Fenbert', 'student_number' => '2025316', 'email' => 'afenbert25@toledosua.org', 'password' => Hash::make('25316His^'),]);
+        DB::table('users')->insert(['first_name' => 'Jacqueline', 'last_name' => 'Freeman', 'student_number' => '2025317', 'email' => 'jfreeman25@toledosua.org', 'password' => Hash::make('25317Tot*'),]);
+        DB::table('users')->insert(['first_name' => 'Kendall', 'last_name' => 'Freeworth', 'student_number' => '2025318', 'email' => 'kfreeworth25@toledosua.org', 'password' => Hash::make('25318Hit*'),]);
+        DB::table('users')->insert(['first_name' => 'Avery', 'last_name' => 'Grimes', 'student_number' => '2025319', 'email' => 'agrimes25@toledosua.org', 'password' => Hash::make('25319Jog&'),]);
+        DB::table('users')->insert(['first_name' => 'Lily', 'last_name' => 'Grissom', 'student_number' => '2025320', 'email' => 'lgrissom25@toledosua.org', 'password' => Hash::make('25320How#'),]);
+        DB::table('users')->insert(['first_name' => 'June', 'last_name' => 'Hansen', 'student_number' => '2025321', 'email' => 'jhansen25@toledosua.org', 'password' => Hash::make('25321Soy$'),]);
+        DB::table('users')->insert(['first_name' => 'Mackenzie', 'last_name' => 'Heban', 'student_number' => '2025322', 'email' => 'mheban25@toledosua.org', 'password' => Hash::make('25322Wow%'),]);
+        DB::table('users')->insert(['first_name' => 'Marissa', 'last_name' => 'Holman', 'student_number' => '2025323', 'email' => 'mholman25@toledosua.org', 'password' => Hash::make('25323Leg%'),]);
+        DB::table('users')->insert(['first_name' => 'Claire', 'last_name' => 'Hubbell', 'student_number' => '2025324', 'email' => 'chubbell25@toledosua.org', 'password' => Hash::make('25324Nun!'),]);
+        DB::table('users')->insert(['first_name' => 'Kendall', 'last_name' => 'Jankowski', 'student_number' => '2025325', 'email' => 'kjankowski25@toledosua.org', 'password' => Hash::make('25325Nay!'),]);
+        DB::table('users')->insert(['first_name' => 'Emma', 'last_name' => 'Jones', 'student_number' => '2025326', 'email' => 'ejones25@toledosua.org', 'password' => Hash::make('25326Dub*'),]);
+        DB::table('users')->insert(['first_name' => 'Addison', 'last_name' => 'Keiffer', 'student_number' => '2025327', 'email' => 'akeiffer25@toledosua.org', 'password' => Hash::make('25327Pac*'),]);
+        DB::table('users')->insert(['first_name' => 'Isadora', 'last_name' => 'Kober', 'student_number' => '2025328', 'email' => 'ikober25@toledosua.org', 'password' => Hash::make('25328Rue%'),]);
+        DB::table('users')->insert(['first_name' => 'Lily', 'last_name' => 'Koepfler', 'student_number' => '2025372', 'email' => 'lkoepfler25@toledosua.org', 'password' => Hash::make('25372Mat@'),]);
+        DB::table('users')->insert(['first_name' => 'Sierra', 'last_name' => 'Kolcun', 'student_number' => '2025329', 'email' => 'skolcun25@toledosua.org', 'password' => Hash::make('25329Cam#'),]);
+        DB::table('users')->insert(['first_name' => 'Kelsie', 'last_name' => 'Koop', 'student_number' => '2025330', 'email' => 'kkoop25@toledosua.org', 'password' => Hash::make('25330Put^'),]);
+        DB::table('users')->insert(['first_name' => 'Lauren', 'last_name' => 'Limpert', 'student_number' => '2025331', 'email' => 'llimpert25@toledosua.org', 'password' => Hash::make('25331Bat$'),]);
+        DB::table('users')->insert(['first_name' => 'Mireya', 'last_name' => 'Lopez', 'student_number' => '2025332', 'email' => 'mlopez25@toledosua.org', 'password' => Hash::make('25332Bob*'),]);
+        DB::table('users')->insert(['first_name' => 'Audrey', 'last_name' => 'Lorigan', 'student_number' => '2025333', 'email' => 'alorigan25@toledosua.org', 'password' => Hash::make('25333For^'),]);
+        DB::table('users')->insert(['first_name' => 'Sydney', 'last_name' => 'Miller', 'student_number' => '2025334', 'email' => 'smiller25@toledosua.org', 'password' => Hash::make('25334Put@'),]);
+        DB::table('users')->insert(['first_name' => 'Kate', 'last_name' => 'Moran', 'student_number' => '2025335', 'email' => 'kmoran25@toledosua.org', 'password' => Hash::make('25335Tee#'),]);
+        DB::table('users')->insert(['first_name' => 'Emma', 'last_name' => 'Myers', 'student_number' => '2025336', 'email' => 'emyers25@toledosua.org', 'password' => Hash::make('25336Dry*'),]);
+        DB::table('users')->insert(['first_name' => 'Briana', 'last_name' => 'Nowak', 'student_number' => '2025337', 'email' => 'bnowak25@toledosua.org', 'password' => Hash::make('25337Par*'),]);
+        DB::table('users')->insert(['first_name' => 'Ruby', 'last_name' => 'Obarski', 'student_number' => '2025338', 'email' => 'robarski25@toledosua.org', 'password' => Hash::make('25338Kit*'),]);
+        DB::table('users')->insert(['first_name' => 'Shelby', 'last_name' => 'Ohms', 'student_number' => '2025339', 'email' => 'sohms25@toledosua.org', 'password' => Hash::make('25339App*'),]);
+        DB::table('users')->insert(['first_name' => 'Vanessa', 'last_name' => 'Onyekelu', 'student_number' => '2025340', 'email' => 'vonyekelu25@toledosua.org', 'password' => Hash::make('25340Tab#'),]);
+        DB::table('users')->insert(['first_name' => 'Ellie', 'last_name' => 'Puckett', 'student_number' => '2025341', 'email' => 'epuckett25@toledosua.org', 'password' => Hash::make('25341Par!'),]);
+        DB::table('users')->insert(['first_name' => 'Sienna', 'last_name' => 'Rains', 'student_number' => '2025342', 'email' => 'srains25@toledosua.org', 'password' => Hash::make('25342Ala%'),]);
+        DB::table('users')->insert(['first_name' => 'Abigail', 'last_name' => 'Ralph', 'student_number' => '2025343', 'email' => 'aralph25@toledosua.org', 'password' => Hash::make('25343Sim!'),]);
+        DB::table('users')->insert(['first_name' => 'Madison', 'last_name' => 'Repass', 'student_number' => '2025344', 'email' => 'mrepass25@toledosua.org', 'password' => Hash::make('25344Mon!'),]);
+        DB::table('users')->insert(['first_name' => 'Sasha', 'last_name' => 'Reynolds', 'student_number' => '2025345', 'email' => 'sreynolds25@toledosua.org', 'password' => Hash::make('25345See$'),]);
+        DB::table('users')->insert(['first_name' => 'Rachael', 'last_name' => 'Roberts', 'student_number' => '2025346', 'email' => 'rroberts25@toledosua.org', 'password' => Hash::make('25346Lib%'),]);
+        DB::table('users')->insert(['first_name' => 'Jennifer', 'last_name' => 'Sautter', 'student_number' => '2025347', 'email' => 'jsautter25@toledosua.org', 'password' => Hash::make('25347Meg^'),]);
+        DB::table('users')->insert(['first_name' => 'Hope', 'last_name' => 'Seman', 'student_number' => '2025348', 'email' => 'hseman25@toledosua.org', 'password' => Hash::make('25348Bot%'),]);
+        DB::table('users')->insert(['first_name' => 'McKenna', 'last_name' => 'Seman', 'student_number' => '2025349', 'email' => 'mseman25@toledosua.org', 'password' => Hash::make('25349Far*'),]);
+        DB::table('users')->insert(['first_name' => 'Avery', 'last_name' => 'Shoots', 'student_number' => '2025350', 'email' => 'ashoots25@toledosua.org', 'password' => Hash::make('25350Mud*'),]);
+        DB::table('users')->insert(['first_name' => 'Madison', 'last_name' => 'Skala', 'student_number' => '2025351', 'email' => 'mskala25@toledosua.org', 'password' => Hash::make('25351Jet%'),]);
+        DB::table('users')->insert(['first_name' => 'Delaney', 'last_name' => 'Smith', 'student_number' => '2025352', 'email' => 'dsmith25@toledosua.org', 'password' => Hash::make('25352Pap&'),]);
+        DB::table('users')->insert(['first_name' => 'Kimora', 'last_name' => 'Smith', 'student_number' => '2025353', 'email' => 'ksmith25@toledosua.org', 'password' => Hash::make('25353Pat$'),]);
+        DB::table('users')->insert(['first_name' => 'Grace', 'last_name' => 'Spenthoff', 'student_number' => '2025354', 'email' => 'gspenthoff25@toledosua.org', 'password' => Hash::make('25354Rib#'),]);
+        DB::table('users')->insert(['first_name' => 'Ella', 'last_name' => 'Stoll', 'student_number' => '2025355', 'email' => 'estoll25@toledosua.org', 'password' => Hash::make('25355Elm%'),]);
+        DB::table('users')->insert(['first_name' => 'Rachel', 'last_name' => 'Stuller', 'student_number' => '2025356', 'email' => 'rstuller25@toledosua.org', 'password' => Hash::make('25356Sea@'),]);
+        DB::table('users')->insert(['first_name' => 'Samantha', 'last_name' => 'Sugg', 'student_number' => '2025357', 'email' => 'ssugg25@toledosua.org', 'password' => Hash::make('25357Who%'),]);
+        DB::table('users')->insert(['first_name' => 'Vivian', 'last_name' => 'Suhy', 'student_number' => '2025367', 'email' => 'vsuhy25@toledosua.org', 'password' => Hash::make('25367Sup@'),]);
+        DB::table('users')->insert(['first_name' => 'Janelle', 'last_name' => 'Szymanski', 'student_number' => '2025358', 'email' => 'jszymanski25@toledosua.org', 'password' => Hash::make('25358Cap@'),]);
+        DB::table('users')->insert(['first_name' => 'Alyssa', 'last_name' => 'Taylor', 'student_number' => '2025374', 'email' => 'ataylor25@toledosua.org', 'password' => Hash::make('25374Zip%'),]);
+        DB::table('users')->insert(['first_name' => 'Joy', 'last_name' => 'Tenkorang', 'student_number' => '2025373', 'email' => 'jtenkorang25@toledosua.org', 'password' => Hash::make('25373Cob#'),]);
+        DB::table('users')->insert(['first_name' => 'Eleanor', 'last_name' => 'Tuck', 'student_number' => '2025359', 'email' => 'etuck25@toledosua.org', 'password' => Hash::make('25359Bud!'),]);
+        DB::table('users')->insert(['first_name' => 'Grace', 'last_name' => 'Tuori', 'student_number' => '2025360', 'email' => 'gtuori25@toledosua.org', 'password' => Hash::make('25360The^'),]);
+        DB::table('users')->insert(['first_name' => 'Emerson', 'last_name' => 'Turner', 'student_number' => '2025361', 'email' => 'eturner25@toledosua.org', 'password' => Hash::make('25361Ban&'),]);
+        DB::table('users')->insert(['first_name' => 'Audrey', 'last_name' => 'Vendemo', 'student_number' => '2025362', 'email' => 'avendemo25@toledosua.org', 'password' => Hash::make('25362Dug*'),]);
+        DB::table('users')->insert(['first_name' => 'Ashley', 'last_name' => 'Wagner', 'student_number' => '2025363', 'email' => 'awagner25@toledosua.org', 'password' => Hash::make('25363Sis&'),]);
+        DB::table('users')->insert(['first_name' => 'Brynn', 'last_name' => 'Walsh', 'student_number' => '2025364', 'email' => 'bwalsh25@toledosua.org', 'password' => Hash::make('25364Dig$'),]);
+        DB::table('users')->insert(['first_name' => 'Zoe', 'last_name' => 'Werner', 'student_number' => '2025365', 'email' => 'zwerner25@toledosua.org', 'password' => Hash::make('25365Got%'),]);
+        DB::table('users')->insert(['first_name' => 'Charlee', 'last_name' => 'Wood', 'student_number' => '2025366', 'email' => 'cwood25@toledosua.org', 'password' => Hash::make('25366Rug@'),]);
+        DB::table('users')->insert(['first_name' => 'Miranda', 'last_name' => 'Lydey', 'student_number' => '2024401', 'email' => 'mlydey24@toledosua.org', 'password' => Hash::make('24401Cab#'),]);
+        DB::table('users')->insert(['first_name' => 'Sydney', 'last_name' => 'Peters', 'student_number' => '2024400', 'email' => 'slpeters24@toledosua.org', 'password' => Hash::make('24400Moe%'),]);
+    }
+}
